01c84894e221d75d7751194554d64353c39fb927
Leonid Deev <ltupov2003@yandex.ru>
2023-04-27 22:47:07 +0300

    Add Image

--- src/sections/FirstSection/FirstSection.jsx
+++ src/sections/FirstSection/FirstSection.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Style from "./FirstSection.module.css";
+import classNames from "classnames";
+import Communism from "./../../image/communism_is_dead.jpg";
+import Education from "./../../image/education.jpg";
+import Park from "./../../image/park.jpg";
+import Museum from "./../../image/golden_museum.jpg";
 
 export const FirstSection = () => {
     return (
@@ -7,12 +12,20 @@ export const FirstSection = () => {
             <div></div>
             <div>
                 <div className={Style.Grid}>
-                    <div className={Style.GridCard}></div>
-                    <div className={Style.GridCard}></div>
-                    <div className={Style.GridCard}></div>
-                    <div className={Style.GridCard}></div>
-                    <div className={Style.GridCard}></div>
-                    <div className={Style.GridCard}></div>
+                    <div className={classNames(Style.GridCard)}></div>
+                    <div className={classNames(Style.GridCard)}>
+                        <img src={Museum} alt={"Мероприятия, организованные при поддержке Департамента культуры города Москвы"}/>
+                    </div>
+                    <div className={classNames(Style.GridCard)}>
+                        <img src={Education} alt={"Образовательные мероприятия Учебного центра Мосгосэкспертизы"}/>
+                    </div>
+                    <div className={classNames(Style.GridCard)}>
+                        <img src={Communism} alt={"Мероприятия по установке подъемных платформ для маломобильных групп населения в рамках реализации Государственной программы «Социальная поддержка жителей города Москвы»"}/>
+                    </div>
+                    <div className={classNames(Style.GridCard)}>
+                        <img src={Park} alt={"Мероприятия по созданию и благоустройству парков и озелененных территорий"}/>
+                    </div>
+                    <div className={classNames(Style.GridCard)}></div>
                 </div>
             </div>
         </section>
