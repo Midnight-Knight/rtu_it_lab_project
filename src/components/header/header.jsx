5ecbb01af886af9190e71852692f03eb5174f2a6
Leonid Deev <ltupov2003@yandex.ru>
2023-04-28 11:32:21 +0300

    Create header jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Style from "./header.module.css";
+import {ButtonLogo, ButtonText} from "../buttons/buttons";
 
 export const Header = () => {
     return (
         <header className={Style.Header}>
-
+            <ButtonLogo func={null}/>
+            <nav>
+                <ButtonText text={"Культура"} func={null}/>
+                <ButtonText text={"Образование"} func={null}/>
+                <ButtonText text={"Доступная среда"} func={null}/>
+                <ButtonText text={"Благоустройство"} func={null}/>
+                <ButtonText text={"Кабинет"} func={null}/>
+            </nav>
         </header>
     )
 }
--- src/sections/MainSection/MainSection.jsx
+++ src/sections/MainSection/MainSection.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Style from "./MainSection.module.css";
-import classNames from "classnames";
-import Communism from "./../../image/communism_is_dead.jpg";
-import Education from "./../../image/education.jpg";
-import Park from "./../../image/park.jpg";
-import Museum from "./../../image/golden_museum.jpg";
+//import classNames from "classnames";
+//import Communism from "./../../image/communism_is_dead.jpg";
+//import Education from "./../../image/education.jpg";
+//import Park from "./../../image/park.jpg";
+//import Museum from "./../../image/golden_museum.jpg";
 
 export const MainSection = () => {
     return (
