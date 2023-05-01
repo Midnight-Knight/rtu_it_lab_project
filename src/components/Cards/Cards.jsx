675504dc460eca0bf14802d803e7e30cba35caa3
Leonid Deev <ltupov2003@yandex.ru>
2023-05-01 15:21:55 +0300

    Create Broadcast in MainSection

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Style from "./Cards.module.css";
+import photo1 from "../../image/photo1.png";
+import photo2 from "../../image/photo2.png";
+import photo3 from "../../image/photo3.png";
+import Sport from "../../image/sport.png";
+import building from "../../image/building.png";
+import calendar from "../../image/calendary.png";
+
+export const Category = () => {
+    return (
+        <div className={Style.GridDiv}>
+            <div className={Style.GridCard}>
+                <img src={photo1}/>
+                <h4>Мероприятия, организованные при поддержке Департамента культуры города Москвы</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img src={photo2}/>
+                <h4>Образовательных мероприятий Учебного центра Мосгосэкспертизы</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img src={photo3}/>
+                <h4>Туристский событийный календарь города Москвы</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img src={Sport}/>
+                <h4>Перечень спортивных мероприятий</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img src={building}/>
+                <h4>Торжественные мероприятия в честь завершения строительства или реставрации общественных объектов</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img src={calendar}/>
+                <h4>Личный кабинет</h4>
+            </div>
+        </div>
+    )
+}
--- src/sections/MainSection/MainSection.jsx
+++ src/sections/MainSection/MainSection.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import Style from "./MainSection.module.css";
-//import classNames from "classnames";
-import photo1 from "./../../image/photo1.png";
-import photo2 from "./../../image/photo2.png";
-import photo3 from "./../../image/photo3.png";
-import Sport from "./../../image/sport.png";
-import calendar from "./../../image/calendary.png";
-import building from "./../../image/building.png";
 import {MyMap} from "../../components/MyMap/MyMap";
+import {Category} from "../../components/Cards/Cards";
 
 export const MainSection = () => {
 
@@ -23,36 +17,17 @@ export const MainSection = () => {
             <div>
                 <h1>Категории</h1>
                 <h4>Найдите то что вам интересно или посмотрите сохранённое в личном кабинете.</h4>
-                <div className={Style.GridDiv}>
-                    <div className={Style.GridCard}>
-                        <img src={photo1}/>
-                        <h4>Мероприятия, организованные при поддержке Департамента культуры города Москвы</h4>
-                    </div>
-                    <div className={Style.GridCard}>
-                        <img src={photo2}/>
-                        <h4>Образовательных мероприятий Учебного центра Мосгосэкспертизы</h4>
-                    </div>
-                    <div className={Style.GridCard}>
-                        <img src={photo3}/>
-                        <h4>Туристский событийный календарь города Москвы</h4>
-                    </div>
-                    <div className={Style.GridCard}>
-                        <img src={Sport}/>
-                        <h4>Перечень спортивных мероприятий</h4>
-                    </div>
-                    <div className={Style.GridCard}>
-                        <img src={building}/>
-                        <h4>Торжественные мероприятия в честь завершения строительства или реставрации общественных объектов</h4>
-                    </div>
-                    <div className={Style.GridCard}>
-                        <img src={calendar}/>
-                        <h4>Личный кабинет</h4>
-                    </div>
-                </div>
+                <Category/>
             </div>
             <div>
                 <h1>Интерактивная карта мероприятий</h1>
+                <h4>Карта со всеми категорий мероприятиями и информацией о них.</h4>
                 <MyMap/>
+            </div>
+            <div>
+                <h1>Прямые трансляции</h1>
+                <h4>Подробная информацию о мероприятиях, а также прямые трансляции с места проведения мероприятий.</h4>
+                <div/>
             </div>
         </section>
     )
