525a5c416df2604020762ea500c1bcae5be5cf8f
Leonid Deev <ltupov2003@yandex.ru>
2023-05-01 18:41:31 +0300

    Create footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style/App.css";
 import {Header} from "./components/header/header";
 import {MainSection} from "./sections/MainSection/MainSection";
+import {Footer} from "./components/footer/footer";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header/>
         <MainSection/>
+        <Footer/>
     </div>
   );
 }
