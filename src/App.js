faea7e37d1b959fb531ba5decc061998a8c9bb3e
Leonid Deev <ltupov2003@yandex.ru>
2023-05-01 16:16:53 +0300

    Create Section PA in Main

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React, {useEffect} from "react";
+import React from "react";
 import "./style/App.css";
 import {Header} from "./components/header/header";
 import {MainSection} from "./sections/MainSection/MainSection";
-import axios from "axios";
 
 function App() {
-    useEffect(() => {
-        async function API()
-        {
-            try {
-                const response = await axios.get("https://apidata.mos.ru/v1/datasets/3295?api_key=b7546afced8886fa3b41debca1e78a84");
-                const JsonData = await response.data;
-                console.log(JsonData);
-            }
-            catch (e)
-            {
-                console.log(e);
-            }
-        }
-
-        API();
-    }, []);
-
 
   return (
     <div className="App">
