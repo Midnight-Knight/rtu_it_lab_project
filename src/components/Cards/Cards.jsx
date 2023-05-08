3def76665f34934c125ede45615b139de7b7608d
Leonid Deev <ltupov2003@yandex.ru>
2023-05-09 02:39:32 +0300

    Update Main Page

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 import Style from "./Cards.module.css";
-import photo1 from "../../image/photo1.png";
-import photo2 from "../../image/photo2.png";
-import photo3 from "../../image/photo3.png";
-import Sport from "../../image/sport.png";
-import building from "../../image/building.png";
-import calendar from "../../image/calendary.png";
+import Festivals from "./../../image/festivals.png";
+import Sport from "./../../image/sport.png";
+import Exhibitions from "./../../image/exhibitions.jpg";
+import Art from "./../../image/art.jpg";
+import Citylife from "./../../image/citylife.jpg"
+import Family from "./../../image/family.jpg"
 
 export const Category = () => {
     return (
         <div className={Style.GridDiv}>
             <div className={Style.GridCard}>
-                <img alt="" src={photo1}/>
-                <h4>Мероприятия, организованные при поддержке Департамента культуры города Москвы</h4>
-            </div>
-            <div className={Style.GridCard}>
-                <img alt="" src={photo2}/>
-                <h4>Образовательные мероприятия Учебного центра Мосгосэкспертизы</h4>
-            </div>
-            <div className={Style.GridCard}>
-                <img alt="" src={photo3}/>
-                <h4>Туристский событийный календарь города Москвы</h4>
+                <img alt="" src={Festivals}/>
+                <h4>Фестивали культуры и праздники города</h4>
             </div>
             <div className={Style.GridCard}>
                 <img alt="" src={Sport}/>
-                <h4>Перечень спортивных мероприятий</h4>
+                <h4>Спортивные события и соревнования города</h4>
             </div>
             <div className={Style.GridCard}>
-                <img alt="" src={building}/>
-                <h4>Торжественные мероприятия в честь завершения строительства или реставрации общественных объектов</h4>
+                <img alt="" src={Exhibitions}/>
+                <h4>Профессиональные конференции и выставки города</h4>
             </div>
             <div className={Style.GridCard}>
-                <img alt="" src={calendar}/>
-                <h4>Личный кабинет</h4>
+                <img alt="" src={Art}/>
+                <h4>Художественные выставки и галереи города</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img alt="" src={Citylife}/>
+                <h4>События и фестивали, отражающие культуру и историю города</h4>
+            </div>
+            <div className={Style.GridCard}>
+                <img alt="" src={Family}/>
+                <h4>Детские развлечения и мероприятия города</h4>
             </div>
         </div>
     )
