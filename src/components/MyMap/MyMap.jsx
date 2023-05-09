22cb6f42fcd1ea96b778a193e781a2aee048c99a
Leonid Deev <ltupov2003@yandex.ru>
2023-05-10 00:18:10 +0300

    Update Placemark and Map

--- src/components/MyMap/MyMap.jsx
+++ src/components/MyMap/MyMap.jsx
@@ -1,89 +1,236 @@
 import React, {useEffect, useState} from "react";
-import {Map, Placemark, YMaps} from "@pbe/react-yandex-maps";
+import {
+    Clusterer,
+    FullscreenControl,
+    GeolocationControl,
+    Map,
+    Placemark,
+    YMaps,
+    ZoomControl
+} from "@pbe/react-yandex-maps";
 import Style from "./MyMap.module.css";
-import ImagePlacemarkRedActive from "./../../image/RedPlacemark.svg";
-import ImagePlacemarkRed from "./../../image/PlacemarkBlack1.svg";
-import ImagePlacemarkYellowActive from "./../../image/YellowPlacemark.svg";
-import ImagePlacemarkYellow from "./../../image/PlacemarkBlack2.svg";
-import ImagePlacemarkGreenActive from "./../../image/GreenPlacemark.svg";
-import ImagePlacemarkGreen from "./../../image/PlacemarkBlack3.svg";
-import ImagePlacemarkBlueActive from "./../../image/BluePlacemark.svg";
-import ImagePlacemarkBlue from "./../../image/PlacemarkBlack4.svg";
-import ImagePlacemarkPurpleActive from "./../../image/PurplePlacemark.svg";
-import ImagePlacemarkPurple from "./../../image/PlacemarkBlack5.svg";
+import PlacemarkArtBlack from "./../../image/PlacemarkArtBlack.svg";
+import PlacemarkArtColor from "./../../image/PlacemarkArtColor.svg";
+import PlacemarkFamilyBlack from "./../../image/PlacemarkFamilyBlack.svg";
+import PlacemarkFamilyColor from "./../../image/PlacemarkFamilyColor.svg";
+import PlacemarkSportBlack from "./../../image/PlacemarkSportBlack.svg";
+import PlacemarkSportColor from "./../../image/PlacemarkSportColor.svg";
+import PlacemarkExhibitionsBlack from "./../../image/PlacemarkExhibitionsBlack.svg";
+import PlacemarkExhibitionsColor from "./../../image/PlacemarkExhibitionsColor.svg";
+import PlacemarkFestivalsBlack from "./../../image/PlacemarkFestivalsBlack.svg";
+import PlacemarkFestivalsColor from "./../../image/PlacemarkFestivalsColor.svg";
+import PlacemarkCitylifeBlack from "./../../image/PlacemarkCitylifeBlack.svg";
+import PlacemarkCitylifeColor from "./../../image/PlacemarkCitylifeColor.svg";
 
-const PlacemarkRed = (props) => {
+const PlacemarkArt = (props) => {
     const placemarkOptions = {
         iconLayout: "default#image",
-        iconImageSize: [50, 50],
-        iconImageHref: props.active === true ? ImagePlacemarkRedActive : ImagePlacemarkRed,
+        iconImageSize: [32, 48],
+        iconImageHref: props.active === true ? PlacemarkArtColor : PlacemarkArtBlack,
     }
 
 
     return (
-        <Placemark onClick={props.func} options={placemarkOptions} geometry={props.coordinates}/>
+        <Placemark
+            options={placemarkOptions}
+            geometry={props.coordinates}
+            onClick={props.balloon.func}
+            openBalloon={props.balloon.balloonOpen}
+            onClose={props.balloon.balloonClose}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+            properties={{
+                balloonContentHeader: `<a href=${props.balloon.link}><h6>${props.balloon.title}</h6></a>`,
+                balloonContentBody: `<p>${props.balloon.address}</p><p>${props.balloon.date}</p>`,
+                balloonContentFooter: props.balloon.category,
+                hintContent: props.balloon.title,
+            }}
+            state={{
+                balloonLayout: "default#imageWithContent",
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: true,
+                balloonAutoPan: true,
+                balloonShadow: true,
+            }}
+        />
     )
 }
 
-const PlacemarkYellow = (props) => {
+const PlacemarkFamily = (props) => {
     const placemarkOptions = {
         iconLayout: "default#image",
-        iconImageSize: [50, 50],
-        iconImageHref: props.active === true ? ImagePlacemarkYellowActive : ImagePlacemarkYellow,
+        iconImageSize: [32, 48],
+        iconImageHref: props.active === true ? PlacemarkFamilyColor : PlacemarkFamilyBlack,
     }
 
     return (
-        <Placemark onClick={props.func} options={placemarkOptions} geometry={props.coordinates}/>
+        <Placemark
+            options={placemarkOptions}
+            geometry={props.coordinates}
+            onClick={props.balloon.func}
+            openBalloon={props.balloon.balloonOpen}
+            onClose={props.balloon.balloonClose}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+            properties={{
+                balloonContentHeader: `<a href=${props.balloon.link}><h6>${props.balloon.title}</h6></a>`,
+                balloonContentBody: `<p>${props.balloon.address}</p><p>${props.balloon.date}</p>`,
+                balloonContentFooter: props.balloon.category,
+                hintContent: props.balloon.title,
+            }}
+            state={{
+                balloonLayout: "default#imageWithContent",
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: true,
+                balloonAutoPan: true,
+                balloonShadow: true,
+            }}
+        />
     )
 }
 
-const PlacemarkGreen = (props) => {
+const PlacemarkSport = (props) => {
     const placemarkOptions = {
         iconLayout: "default#image",
-        iconImageSize: [50, 50],
-        iconImageHref: props.active === true ? ImagePlacemarkGreenActive : ImagePlacemarkGreen,
+        iconImageSize: [32, 48],
+        iconImageHref: props.active === true ? PlacemarkSportColor : PlacemarkSportBlack,
     }
 
     return (
-        <Placemark onClick={props.func} options={placemarkOptions} geometry={props.coordinates}/>
+        <Placemark
+            options={placemarkOptions}
+            geometry={props.coordinates}
+            onClick={props.balloon.func}
+            openBalloon={props.balloon.balloonOpen}
+            onClose={props.balloon.balloonClose}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+            properties={{
+                balloonContentHeader: `<a href=${props.balloon.link}><h6>${props.balloon.title}</h6></a>`,
+                balloonContentBody: `<p>${props.balloon.address}</p><p>${props.balloon.date}</p>`,
+                balloonContentFooter: props.balloon.category,
+                hintContent: props.balloon.title,
+            }}
+            state={{
+                balloonLayout: "default#imageWithContent",
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: true,
+                balloonAutoPan: true,
+                balloonShadow: true,
+            }}
+        />
     )
 }
 
-const PlacemarkBlue = (props) => {
+const PlacemarkExhibitions = (props) => {
     const placemarkOptions = {
         iconLayout: "default#image",
-        iconImageSize: [50, 50],
-        iconImageHref: props.active === true ? ImagePlacemarkBlueActive : ImagePlacemarkBlue,
+        iconImageSize: [32, 48],
+        iconImageHref: props.active === true ? PlacemarkExhibitionsColor : PlacemarkExhibitionsBlack,
     }
 
     return (
-        <Placemark onClick={props.func} options={placemarkOptions} geometry={props.coordinates}/>
+        <Placemark
+            options={placemarkOptions}
+            geometry={props.coordinates}
+            onClick={props.balloon.func}
+            openBalloon={props.balloon.balloonOpen}
+            onClose={props.balloon.balloonClose}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+            properties={{
+                balloonContentHeader: `<a href=${props.balloon.link}><h6>${props.balloon.title}</h6></a>`,
+                balloonContentBody: `<p>${props.balloon.address}</p><p>${props.balloon.date}</p>`,
+                balloonContentFooter: props.balloon.category,
+                hintContent: props.balloon.title,
+            }}
+            state={{
+                balloonLayout: "default#imageWithContent",
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: true,
+                balloonAutoPan: true,
+                balloonShadow: true,
+            }}
+        />
     )
 }
 
-const PlacemarkPurple = (props) => {
+const PlacemarkFestivals = (props) => {
     const placemarkOptions = {
         iconLayout: "default#image",
-        iconImageSize: [50, 50],
-        iconImageHref: props.active === true ? ImagePlacemarkPurpleActive : ImagePlacemarkPurple,
+        iconImageSize: [32, 48],
+        iconImageHref: props.active === true ? PlacemarkFestivalsColor : PlacemarkFestivalsBlack,
     }
 
     return (
-        <Placemark className={Style.RedAnimation} onClick={props.func} options={placemarkOptions} geometry={props.coordinates}/>
+        <Placemark
+            options={placemarkOptions}
+            geometry={props.coordinates}
+            onClick={props.balloon.func}
+            openBalloon={props.balloon.balloonOpen}
+            onClose={props.balloon.balloonClose}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+            properties={{
+                balloonContentHeader: `<a href=${props.balloon.link}><h6>${props.balloon.title}</h6></a>`,
+                balloonContentBody: `<p>${props.balloon.address}</p><p>${props.balloon.date}</p>`,
+                balloonContentFooter: props.balloon.category,
+                hintContent: props.balloon.title,
+            }}
+            state={{
+                balloonLayout: "default#imageWithContent",
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: true,
+                balloonAutoPan: true,
+                balloonShadow: true,
+            }}
+        />
     )
 }
 
-export const MyMap = () => {
+const PlacemarkCitylife = (props) => {
+    const placemarkOptions = {
+        iconLayout: "default#image",
+        iconImageSize: [32, 48],
+        iconImageHref: props.active === true ? PlacemarkCitylifeColor : PlacemarkCitylifeBlack,
+    }
+
+    return (
+        <Placemark
+            options={placemarkOptions}
+            geometry={props.coordinates}
+            onClick={props.balloon.func}
+            openBalloon={props.balloon.balloonOpen}
+            onClose={props.balloon.balloonClose}
+            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+            properties={{
+                balloonContentHeader: `<a href=${props.balloon.link}><h6>${props.balloon.title}</h6></a>`,
+                balloonContentBody: `<p>${props.balloon.address}</p><p>${props.balloon.date}</p>`,
+                balloonContentFooter: props.balloon.category,
+                hintContent: props.balloon.title,
+            }}
+            state={{
+                balloonLayout: "default#imageWithContent",
+                balloonPanelMaxMapArea: 0,
+                balloonCloseButton: true,
+                balloonAutoPan: true,
+                balloonShadow: true,
+            }}
+        />
+    )
+}
+
+export const MyMap = (props) => {
     const [MapPlacemark, SetMapPlacemark] = useState([<></>]);
+    const [balloonIsOpen, setBalloonIsOpen] = useState(false);
 
-    const handleClick = () => {
-        alert("Placemark clicked");
+    const handlePlacemarkClick = () => {
+        setBalloonIsOpen(true);
+    };
+
+    const handleBalloonClose = () => {
+        setBalloonIsOpen(false);
     };
 
     const mapData = {
         center: [55.751432, 37.618883],
         zoom: 10,
-        controls: ["zoomControl", "fullscreenControl"],
+        controls: []
     };
 
     const mapOptions = {
@@ -93,22 +240,77 @@ export const MyMap = () => {
         ],
     };
 
-    const Modules = {
-        arrayModules: ["control.ZoomControl", "control.FullscreenControl"],
+    const clustererOptions = {
+        preset: "islands#invertedVioletClusterIcons",
+        groupByCoordinates: false,
+        clusterIconColor: '#E01B1B'
     }
 
     useEffect(() => {
-        SetMapPlacemark([<PlacemarkRed key="1pl" active={true} func={handleClick} coordinates={[55.76, 37.64]}/>,
-            <PlacemarkYellow key="2pl" active={true} func={handleClick} coordinates={[55.71, 37.64]}/>,
-            <PlacemarkGreen key="3pl" active={true} func={handleClick} coordinates={[55.81, 37.64]}/>,
-            <PlacemarkBlue key="4pl" active={true} func={handleClick} coordinates={[55.76, 37.54]}/>,
-            <PlacemarkPurple key="5pl" active={true} func={handleClick} coordinates={[55.76, 37.74]}/>,
-            <PlacemarkRed key="6pl" func={handleClick} coordinates={[55.76, 37.84]}/>,
-            <PlacemarkYellow key="7pl" func={handleClick} coordinates={[55.71, 37.54]}/>,
-            <PlacemarkGreen key="8pl" func={handleClick} coordinates={[55.71, 37.74]}/>,
-            <PlacemarkBlue key="9pl" func={handleClick} coordinates={[55.81, 37.74]}/>,
-            <PlacemarkPurple key="10pl" func={handleClick} coordinates={[55.81, 37.54]}/>]);
+        SetMapPlacemark([
+            <PlacemarkFamily key="1pl" coordinates={[55.782301, 37.615136]}
+                             balloon={{
+                                 balloonOpen: balloonIsOpen,
+                                 balloonClose: handleBalloonClose,
+                                 func: handlePlacemarkClick,
+                                 link: `/sport`,
+                                 title: "ла",
+                                 address: "address",
+                                 category: "category",
+                                 date: "date"}}/>,
+            <PlacemarkArt key="2pl" coordinates={[55.802411, 37.635246]}
+                             balloon={{
+                                 balloonOpen: balloonIsOpen,
+                                 balloonClose: handleBalloonClose,
+                                 func: handlePlacemarkClick,
+                                 link: `/sport`,
+                                 title: "ла",
+                                 address: "address",
+                                 category: "category",
+                                 date: "date"}}/>,
+            <PlacemarkFestivals key="3pl" coordinates={[55.782191, 37.625026]}
+                             balloon={{
+                                 balloonOpen: balloonIsOpen,
+                                 balloonClose: handleBalloonClose,
+                                 func: handlePlacemarkClick,
+                                 link: `/sport`,
+                                 title: "ла",
+                                 address: "address",
+                                 category: "category",
+                                 date: "date"}}/>,
+            <PlacemarkExhibitions key="4pl" coordinates={[55.792411, 37.615136]}
+                             balloon={{
+                                 balloonOpen: balloonIsOpen,
+                                 balloonClose: handleBalloonClose,
+                                 func: handlePlacemarkClick,
+                                 link: `/sport`,
+                                 title: "ла",
+                                 address: "address",
+                                 category: "category",
+                                 date: "date"}}/>,
+            <PlacemarkSport key="5pl" coordinates={[55.802301, 37.625246]}
+                             balloon={{
+                                 balloonOpen: balloonIsOpen,
+                                 balloonClose: handleBalloonClose,
+                                 func: handlePlacemarkClick,
+                                 link: `/sport`,
+                                 title: "ла",
+                                 address: "address",
+                                 category: "category",
+                                 date: "date"}}/>,
+            <PlacemarkCitylife key="6pl" coordinates={[55.792201, 37.625126]}
+                             balloon={{
+                                 balloonOpen: balloonIsOpen,
+                                 balloonClose: handleBalloonClose,
+                                 func: handlePlacemarkClick,
+                                 link: `/sport`,
+                                 title: "ла",
+                                 address: "address",
+                                 category: "category",
+                                 date: "date"}}/>]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+
 
 
     return (
@@ -117,8 +319,13 @@ export const MyMap = () => {
                    query={{
                        apikey: "2d497516-211b-4791-9eb2-bc59d4ea43ee",
                    }}>
-                <Map width={"100%"} height={"100%"} options={mapOptions} state={mapData} modules={Modules.arrayModules}>
-                    {MapPlacemark.map(elem => {return elem})}
+                <Map width={"100%"} height={"100%"} options={mapOptions} state={mapData}>
+                    <Clusterer options={clustererOptions}>
+                        {MapPlacemark.map(elem => {return elem})}
+                    </Clusterer>
+                    <GeolocationControl options={{ float: "left" }} />
+                    <FullscreenControl options={{ float: "right"}}/>
+                    <ZoomControl options={{ float: "left" }}/>
                 </Map>
             </YMaps>
         </div>
