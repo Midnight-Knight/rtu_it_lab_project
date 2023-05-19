cf596f56478611618b5f6e6d9d7ba469643d395f
Leonid Deev <ltupov2003@yandex.ru>
2023-05-20 00:10:50 +0300

    Clear Files

--- src/components/buttons/buttons.jsx
+++ src/components/buttons/buttons.jsx
@@ -32,22 +32,6 @@ export const ButtonNav = (props) => {
     )
 }
 
-export const ButtonFilled = (props) => {
-    return (
-        <button className={classNames(Style.Button, Style.ButtonFilled)} onClick={props.func}>
-            <h6>{props.text}</h6>
-        </button>
-    )
-}
-
-export const ButtonOutlined = (props) => {
-    return (
-        <button className={classNames(Style.Button, Style.ButtonOutlined)} onClick={props.func}>
-            <h6>{props.text}</h6>
-        </button>
-    )
-}
-
 export const ButtonText = (props) => {
     return (
         <button className={classNames(props.widthAuto === true ? Style.ButtonWidthAuto : Style.Button, Style.ButtonText)} onClick={props.func}>
@@ -68,22 +52,6 @@ export const ButtonTextH4 = (props) => {
     return (
         <button className={classNames(props.widthAuto === true ? Style.ButtonWidthAuto : Style.Button, Style.ButtonText)} onClick={props.func}>
             <h4>{props.text}</h4>
-        </button>
-    )
-}
-
-export const ButtonElevated = (props) => {
-    return (
-        <button className={classNames(Style.Button, Style.ButtonElevated)} onClick={props.func}>
-            <h6>{props.text}</h6>
-        </button>
-    )
-}
-
-export const ButtonTonal = (props) => {
-    return (
-        <button className={classNames(Style.Button, Style.ButtonTonal)} onClick={props.func}>
-            <h6>{props.text}</h6>
         </button>
     )
 }
